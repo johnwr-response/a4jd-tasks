c6000201357eb0df93b8308b1f5e4c73e7282d02
johnwr-response <52628290+johnwr-response@users.noreply.github.com>
2020-04-17 20:58:26 +0200

    Task Save

--- src/main/frontend/src/app/tasks/task.service.ts
+++ src/main/frontend/src/app/tasks/task.service.ts
@@ -1,6 +1,7 @@
 import {HttpClient} from "@angular/common/http";
 import {Injectable} from "@angular/core";
 import {Observable} from "rxjs";
+import {Task} from "./task.model";
 
 @Injectable()
 export class TaskService {
@@ -10,6 +11,11 @@ export class TaskService {
 
     getTasks(): Observable<any> {
         return this.http.get('/api/tasks'); // .map(response => response.json())
+    }
+
+    saveTask(task: Task, checked: boolean) {
+        task.completed = checked;
+        return this.http.post('/api/tasks/save', task); // .map(response => response.json())
     }
 
 }
