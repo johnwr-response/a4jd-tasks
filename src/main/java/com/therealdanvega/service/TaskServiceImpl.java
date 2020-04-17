79b69f72cf2e52cfe5a23fd7cefd406d2fa0efb8
johnwr-response <52628290+johnwr-response@users.noreply.github.com>
2020-04-17 16:59:14 +0200

    Spring Boot Data

--- src/main/java/com/therealdanvega/service/TaskServiceImpl.java
+++ src/main/java/com/therealdanvega/service/TaskServiceImpl.java
@@ -17,4 +17,9 @@ public class TaskServiceImpl implements TaskService {
     public Iterable<Task> list() {
         return this.taskRepository.findAll();
     }
+
+    @Override
+    public Task save(Task task) {
+        return this.taskRepository.save(task);
+    }
 }
